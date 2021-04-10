e784032add9d124d99d803dc88b1770946f5e413
alexandru_mandea <alexandru7300@gmail.com>
2021-04-10 16:07:49 +0300

    I've created zodiac.proto and ZodiacSignTellerService class and built the service project.

--- ZodiacService/ZodiacService/Startup.cs
+++ ZodiacService/ZodiacService/Startup.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ZodiacService.Services;
 
 namespace ZodiacService
 {
@@ -31,7 +32,8 @@ namespace ZodiacService
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGrpcService<GreeterService>();
+                //endpoints.MapGrpcService<GreeterService>();
+                endpoints.MapGrpcService<ZodiacSignTellerService>();
 
                 endpoints.MapGet("/", async context =>
                 {
