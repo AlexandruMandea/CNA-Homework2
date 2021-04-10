87c911d3a97cf7407642bfbbaf74e639ab00dc6f
alexandru_mandea <alexandru7300@gmail.com>
2021-04-10 18:43:49 +0300

    I've implemented an utility class with needed utilities (to get zodiac sign from file, for the moment).

---
+++ ZodiacService/ZodiacService/Utilities/Utility.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace ZodiacService.Utilities
+{
+    public class Utility
+    {
+        public static string GetZodiacSign(string date, string filePath)
+        {
+            XmlReader reader = XmlReader.Create(@filePath);
+
+            string[] dateParts = date.Split("/");
+            int month = Int32.Parse(dateParts[0]);
+            int day = Int32.Parse(dateParts[1]);
+            int year = Int32.Parse(dateParts[2]);
+
+            DateTime dateRecieved = new DateTime(year, month, day);
+
+            //using (XmlReader reader = XmlReader.Create(@filePath));
+            //{
+            while (reader.Read())
+            {
+                if (reader.IsStartElement())
+                {
+                    switch (reader.Name.ToString())
+                    {
+                        case "Sign":
+                            string dateRead = reader.GetAttribute(1);
+                            string[] dateReadParts = dateRead.Split(new char[] { '/', '-' });
+                            DateTime intervalStart = new DateTime(year, Int32.Parse(dateReadParts[0]), Int32.Parse(dateReadParts[1]));
+                            DateTime intervalEnd = new DateTime(year, Int32.Parse(dateReadParts[2]), Int32.Parse(dateReadParts[3]));
+                            if (dateRecieved >= intervalStart && dateRecieved <= intervalEnd)
+                            {
+                                return reader.GetAttribute(0);
+                            }
+                            break;
+                    }
+                }
+            }
+            //}
+            return "*null*";
+        }
+    }
+}
